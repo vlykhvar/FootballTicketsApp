0efdce0ce2131984e3310efbb2256e4fef6a3218
Volodymyr Lykhvar <vlykhvar@gmail.com>
2021-01-26 16:48:27 +0200

    added for test

--- src/main/java/com/dev/theater/Main.java
+++ src/main/java/com/dev/theater/Main.java
@@ -1,7 +1,19 @@
 package com.dev.theater;
 
-public class Main {
-    public static void main(String[] args) {
+import com.dev.theater.library.Injector;
+import com.dev.theater.model.Movie;
+import com.dev.theater.service.MovieService;
+import com.dev.theater.service.impl.MovieServiceImpl;
 
+public class Main {
+    private static Injector injector = Injector.getInstance(Main.class.getPackageName());
+
+    public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.setTitle("Fast and Furious");
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        movieService.add(movie);
+
+        movieService.getAll().forEach(System.out::println);
     }
 }
