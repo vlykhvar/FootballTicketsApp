b854c57a97962a9b06f9e9ce2a64c444264e1880
Volodymyr Lykhvar <vlykhvar@gmail.com>
2021-02-15 15:22:11 +0200

    removed the line that everyone hates, but I still love #4

--- src/main/java/com/dev/theater/service/impl/MovieSessionServiceImpl.java
+++ src/main/java/com/dev/theater/service/impl/MovieSessionServiceImpl.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MovieSessionServiceImpl implements MovieSessionService {
-
     private MovieSessionDao movieSessionDao;
 
     @Autowired
