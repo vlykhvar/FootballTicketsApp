dc1a6e39e8e8b195e6b505387f23e0dc85ef26e4
Volodymyr Lykhvar <vlykhvar@gmail.com>
2021-02-15 15:53:21 +0200

    changed by Ksenia's request 2

--- src/main/java/com/dev/theater/dao/impl/DaoImpl.java
+++ src/main/java/com/dev/theater/dao/impl/DaoImpl.java
@@ -8,7 +8,7 @@ import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class DaoImpl<T> implements GenericDao<T> {
+public abstract class DaoImpl<T> implements GenericDao<T> {
     protected final SessionFactory sessionFactory;
 
     public DaoImpl(SessionFactory sessionFactory) {
